c0e30855885939416388a27cfd51f9cfd8646116
McBryan <solomonmcbryan@gmail.com>
2020-04-25 12:23:36 +0000

    Done

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Login from './Login'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Login/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
